1fe0d57d9b253312e8b6205eaab0f66ceb94c394
sowmya059 <sowmya.murali@dymensions.io>
2023-06-05 23:03:34 +0530

    updated

---
+++ index.js
@@ -0,0 +1,17 @@
+let age = 54
+
+if(age<6){
+    console.log("free")
+}
+else if(age<18){
+    console.log("child discount")
+}
+else if(age<27){
+    console.log("student discount")
+}
+else if(age<67){
+    console.log("full price")
+}
+else{
+    console.log("senior citizen discount")
+}
